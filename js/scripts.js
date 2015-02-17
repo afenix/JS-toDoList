109c8ec75e8fc8a21307625320e5b9c42dfee0bf
Shimran George & Alister Fenix <student@epicodus.com>
2015-02-16 16:41:48 -0800

    Add initial files

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  $('form#tasks').submit(function(event) {
+    event.preventDefault();
+
+    var inputTaskName = $('input#task-name').val();
+    var newTask = {taskName: inputTaskName};
+
+    $("ul#task-list").append("<li><span class='task'>" + newTask.taskName + "</span></li>");
+    $("input#task-name").val("");
+
+
+      $(".task").last().click(function() {
+        $("#show-tasks").show();
+        $(".task-name").text(newTask.taskName + " (Check off if complete)");
+
+
+
+        $('#checkbox').click(function() {
+         if($(this).is(':checked'))
+           {
+        $("ul#completed-tasks").append("<li><span class='task'>" + newTask.taskName + "</span></li>");
+        $("#completed-tasks").show();
+        $(".task-name").text(newTask.taskName);
+      }
+      });
+    });
+  });
+});
